f3a1281c9dd9e5604dd76bb341a65899116f6c6f
Archit Rungta <rungtarchit1@gmail.com>
2020-08-16 00:15:25 +0530

    fixed datatypes

--- src/main.py
+++ src/main.py
@@ -2,11 +2,14 @@
 import rospy
 from gennav_ros.commander import Commander
 from gennav_ros.controller import Controller
+from gennav.utils import RobotState
 
 planner=rospy.get_param("planner")
 env_name=rospy.get_param("env_name")
 goal=rospy.get_param("goal")
+goal=RobotState(position=goal)
 start=rospy.get_param("start")
+start=RobotState(position=start)
 commander=Commander(planner,env_name)
 commander.goto(goal,start)
 controller=Controller()
