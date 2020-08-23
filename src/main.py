e64707a76ffd9679adba33c01d52d09529f22f92
Archit Rungta <rungtarchit1@gmail.com>
2020-08-23 17:11:48 +0530

    updated

--- src/main.py
+++ src/main.py
@@ -8,11 +8,37 @@ from gennav.utils.samplers import *
 from gennav_ros.commander import Commander
 from gennav_ros.controller import Controller
 
+def planner_args():
+    param_list=rospy.get_param_names()
+    for param in param_list:
+        param_list[param_list.index(param)]=param[1:]
+    param_dict={}
+    chk=eval(planner_name+".__init__.__code__.co_varnames")
+    for param in param_list:
+        if param in chk:
+            param_dict[param]=rospy.get_param(param)
+    return param_dict
+
+def env_args():
+    param_list=rospy.get_param_names()
+    for param in param_list:
+        param_list[param_list.index(param)]=param[1:]
+    print param_list
+    param_dict={}
+    chk=eval(env_name+".__init__.__code__.co_varnames")
+    for param in param_list:
+        if param in chk:
+            param_dict[param]=rospy.get_param(param)
+    return param_dict
+
+
 planner_name = rospy.get_param("planner_name")
 env_name = rospy.get_param("env_name")
 obstacles = rospy.get_param("obstacle_data")
-planner = eval(planner_name)
-env = eval(env_name)
+planner_params=planner_args()
+env_params=env_args()
+planner = eval(planner_name+"(**planner_params)")
+env = eval(env_name+"(**env_params)")
 env.update(obstacles)
 commander = Commander(planner, env)
 goal = rospy.get_param("goal")
