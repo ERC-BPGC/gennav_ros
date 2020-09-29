d04bb5d9743d4cd393100a459087bc052ec88451
Suhrudh Sarathy <suhrudhsarathy@gmail.com>
2020-09-29 21:12:52 +0530

    fixed lint issuse :|

--- src/gennav_ros/transformations.py
+++ src/gennav_ros/transformations.py
@@ -1,38 +1,47 @@
 #!/usr/bin/env python
 
 import rospy
-import tf2_ros
-from geometry_msgs.msg import PointStamped
-from gennav.utils.geometry import Point as PT
-from std_msgs.msg import Header
 import tf2_geometry_msgs
+import tf2_ros
+from gennav.utils.geometry import Point as PT
+from geometry_msgs.msg import Point, PointStamped
+from std_msgs.msg import Header
 
 
 class Gennav_Transformer:
-	"""Base class for all the transformations"""
-	def __init__(self):
-		self.tfbuffer = tf2_ros.buffer()
-		self.listener = tf2_ros.TransformListener(self.tfbuffer)
-		self.world_frame = rospy.get_param("world_frame")
-		self.scan_frame = rospy.get_param("robot_frame")
+    """Base class for all the transformations"""
 
-	@classmethod
-	def transform_function_for_polygon_env(cls, msg):
-		"""Transform function for gennav.envs.polygon_env
-		Args:
-			msg([[gennav.common.Point]]) : A list of lists of gennav.utils.geometry.Point
-		Returns:
-			A list of lists of transformed gennav.utils.geometry.Point
-		"""
-		transform = self.tfBuffer.lookup_transform(self.world_frame, self.scan_frame, rospy.Time())
+    def __init__(self):
+        self.tfbuffer = tf2_ros.buffer()
+        self.listener = tf2_ros.TransformListener(self.tfbuffer)
+        self.world_frame = rospy.get_param("world_frame")
+        self.scan_frame = rospy.get_param("robot_frame")
 
-		new_msg = []
-		for list_ in msg:
-			transformed_pts = []
-			for pt in list_ :
-				pointStamped = PointStamped(header=Header(frame_id=self.scan_frame, point = Point(pt.x, pt.y, pt.z)))
-				pointTransformed = tf2_geometry_msgs.do_transform_point(pointStamped, transform)
-				transformed_pts.append(PT(pointTransformed.x, pointTransformed.y, pointTransformed.z))
-			new_msg.append(transformed_pts)
+    @classmethod
+    def transform_function_for_polygon_env(cls, msg):
+        """Transform function for gennav.envs.polygon_env
+        Args:
+        msg : list of lists of gennav.geometry.common.Point
+        """
+        transform = cls.tfBuffer.lookup_transform(
+            cls.world_frame, cls.scan_frame, rospy.Time()
+        )
 
-		return new_msg
+        new_msg = []
+        for list_ in msg:
+            transformed_pts = []
+            for pt in list_:
+                pointStamped = PointStamped(
+                    header=Header(
+                        frame_id=cls.scan_frame, point=Point(pt.x, pt.y, pt.z)
+                    )
+                )
+                pointTransformed = tf2_geometry_msgs.do_transform_point(
+                    pointStamped, transform
+                )
+                transformed_pts.append(
+                    PT(pointTransformed.x, pointTransformed.y, pointTransformed.z)
+                )
+            new_msg.append(transformed_pts)
+
+        return new_msg
